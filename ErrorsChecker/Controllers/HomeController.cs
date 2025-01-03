d329271ce3951f3d471d07cbed5602277f634b4c
Fawa24 <ofeglol@gmail.com>
2025-01-03 19:47:03 +0200

    Upload all the files

---
+++ ErrorsChecker/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using ErrorsChecker.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErrorsChecker.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/analyzer")]
+        public IActionResult AnalyzerResult(string codeToCheck)
+        {
+            var errors = ErrorAnalyzer.CheckSyntaxErrors(codeToCheck);
+
+            return View(new ResultModel { Code = codeToCheck, Errors = errors });
+        }
+    }
+
+    public class ResultModel
+    {
+        public string Code { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
---
+++ ErrorsChecker/Services/ErrorAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace ErrorsChecker.Services
+{
+    public class ErrorAnalyzer
+    {
+        public static List<string> CheckSyntaxErrors(string code)
+        {
+            List<string> errors = new List<string>();
+
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+            var diagnostics = syntaxTree.GetDiagnostics();
+
+            foreach (var diagnostic in diagnostics)
+            {
+                if (diagnostic.Severity == DiagnosticSeverity.Error)
+                {
+                    errors.Add($"Error: {diagnostic.GetMessage()} at line {diagnostic.Location.GetLineSpan().StartLinePosition.Line + 1}");
+                }
+            }
+
+            return errors;
+        }
+    }
+}
